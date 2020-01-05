96bf05b3c1e04392e89834b6345899b721643f46
AWieclawski <a_wieclawski@poczta.pl>
2020-01-05 14:55:54 +0100

    Add moveCellsToTempArray method

--- src/gameOfLifeInclGraphSims/LifeGenerator.java
+++ src/gameOfLifeInclGraphSims/LifeGenerator.java
@@ -5,6 +5,8 @@ public class LifeGenerator {
 	static int LIVE = 1, DEAD = 0, NEIGHBORHOODREVIVE = 3, OVERCROWDED = 3, UNDERPOPULATED = 2;
 
 	public static int[][] getGeneration(int[][] cells, int generations) {
+		
+		int[][] tempArray = moveCellsToTempArray(cells);
 
 		int[][] nextGeneration = newTempArray(cells);
 
@@ -17,6 +19,16 @@ public class LifeGenerator {
 		// than primary array.
 		int[][] tempArray = new int[cells.length + 2][cells[0].length + 2];
 		
+		return tempArray;
+	}
+
+	private static int[][] moveCellsToTempArray(int[][] cells) {
+		int[][] tempArray = newTempArray(cells);
+		for (int y = 0; y < cells.length; y++)
+			for (int x = 0; x < cells[y].length; x++)
+				// put the primary array at center of a new hypothetical research one
+				tempArray[y + 1][x + 1] = cells[y][x];
+
 		return tempArray;
 	}
 
